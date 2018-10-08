620e50671faeb29f70f5d3fd1c1a420c2755eb05
denarellano <37643526+denarellano@users.noreply.github.com>
2018-10-07 18:55:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -12,3 +12,23 @@ data.forEach((UFOsightings) => {
       cell.text(value);
     });
   });
+
+var btn = d3.select('#filter-btn');
+var form = d3.select('form');
+
+btn.on('click', function() {
+    d3.event.preventDefault();
+    
+    var date = d3.select('#datetime').property('value')
+    var filter = tableData.filter (row => row.datetime = date)
+    var tbody = d3.select('tbody');
+    tbody.html('')
+filter.forEach((item) => {
+    var row = tbody.append('tr');
+    Object.entries(item).forEach(([key, value]) => {
+        var cell = tbody.append('td');
+        cell.text(value);
+      });
+    });
+ });
+  
